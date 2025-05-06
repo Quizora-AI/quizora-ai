c1bb92f1edd5342cb2d9004272c27844fabfaee8
wardboy786 <umeshdfd@gmail.com>
2025-05-06 12:42:18 +0530

    Update metro.config.cjs

--- metro.config.cjs
+++ metro.config.cjs
@@ -1,36 +1,32 @@
-
 const { getDefaultConfig } = require('@expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
-module.exports = config;
-  resolver: {
-    assetExts: [...defaultConfig.resolver.assetExts, 'db'],
-    sourceExts: [...defaultConfig.resolver.sourceExts, 'mjs', 'js', 'jsx', 'ts', 'tsx', 'json', 'svg', 'png', 'jpg', 'webp'],
-    extraNodeModules: {
-      'react-native': require.resolve('react-native-web'),
-      'react': require.resolve('react'),
-      'react-dom': require.resolve('react-dom'),
-      'stream': require.resolve('readable-stream'),
-    }
+config.resolver.assetExts.push('db');
+config.resolver.sourceExts.push('mjs', 'js', 'jsx', 'ts', 'tsx', 'json', 'svg', 'png', 'jpg', 'webp');
+config.resolver.extraNodeModules = {
+  'react-native': require.resolve('react-native-web'),
+  'react': require.resolve('react'),
+  'react-dom': require.resolve('react-dom'),
+  'stream': require.resolve('readable-stream'),
+};
+
+config.transformer.getTransformOptions = async () => ({
+  transform: {
+    experimentalImportSupport: false,
+    inlineRequires: true,
   },
-  transformer: {
-    getTransformOptions: async () => ({
-      transform: {
-        experimentalImportSupport: false,
-        inlineRequires: true,
-      },
-    }),
-    assetPlugins: ['expo-asset/tools/hashAssetFiles'],
-  },
-  server: {
-    port: 8081,
-    enhanceMiddleware: (middleware) => {
-      return (req, res, next) => {
-        // Allow connections from all IPs
-        res.setHeader('Access-Control-Allow-Origin', '*');
-        return middleware(req, res, next);
-      };
-    }
+});
+config.transformer.assetPlugins = ['expo-asset/tools/hashAssetFiles'];
+
+config.server = {
+  port: 8081,
+  enhanceMiddleware: (middleware) => {
+    return (req, res, next) => {
+      res.setHeader('Access-Control-Allow-Origin', '*');
+      return middleware(req, res, next);
+    };
   }
 };
+
+module.exports = config;
